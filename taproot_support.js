6940673f87aab6117eca25a50b3b727122554394
mutatrum <mutatrum@gmail.com>
2021-06-16 23:46:07 +0200

    Add piechart image

--- taproot_support.js
+++ taproot_support.js
@@ -8,6 +8,7 @@ const twitter = new Twitter(config.twitter);
 
 const https = require('https');
 const cron = require('node-cron');
+const { createCanvas } = require('canvas');
 
 (async function () {
 
@@ -19,7 +20,6 @@ const cron = require('node-cron');
 async function onSchedule() {
 
   var uaInfo = await getUAInfo();
-
   var piedata = loadData(uaInfo)
   
   logger.log(JSON.stringify(piedata))
@@ -33,14 +33,84 @@ async function onSchedule() {
   var text = `Bitcoin Node Taproot Support: ${percentage.toFixed(2)}%\n`;
   for (var i = 1; i <= percentage; i++) {
     text += '🟧'
+    if (i % 10 == 0) {
+      text += '\n'
+    }
   }
-  text += '\n'
+  text += '\n\nNode count:'
   
   for (var [name, count] of Object.entries(piedata)) {
     text += `\n${name}: ${count}`;
   }
 
-  await twitter.postStatus(text);
+  var buffer = createImage(piedata, total)
+
+  await twitter.postStatus(text, buffer);
+}
+
+function createImage(piedata, total) {
+  const canvas = createCanvas(800, 450);
+  const ctx = canvas.getContext('2d');
+
+  const x = canvas.width / 2;
+  const y = canvas.height / 2;
+
+  ctx.imageSmoothingEnabled = false;
+
+  ctx.beginPath();
+  ctx.rect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = 'white';
+  ctx.fill();
+
+  var gradient_orange = ctx.createRadialGradient(x, y, 175, x, y, 225);
+  gradient_orange.addColorStop(0, '#fa9f1e');
+  gradient_orange.addColorStop(1, '#ee7a21');
+
+  var gradient_blue = ctx.createRadialGradient(x, y, 175, x, y, 225);
+  gradient_blue.addColorStop(0, '#40a2f3');
+  gradient_blue.addColorStop(1, '#1c77d0');
+
+  var gradient_grey = ctx.createRadialGradient(x, y, 175, x, y, 225);
+  gradient_grey.addColorStop(0, '#909090');
+  gradient_grey.addColorStop(1, '#686868');
+
+  var piedataclr = {
+    'Taproot': gradient_orange,
+    'Non-enforcing': gradient_blue,
+    'Light': gradient_grey,
+    'Unknown': gradient_grey
+  };
+  
+  var beginAngle = 0;
+  var endAngle = 0;
+  
+  for(var [name, count] of Object.entries(piedata)) {
+    beginAngle = endAngle;
+    var angle = (count / total) * 2 * Math.PI;
+    endAngle = endAngle + angle;
+    
+    ctx.beginPath();
+    ctx.fillStyle = piedataclr[name];
+    
+    ctx.moveTo(x, y);
+    ctx.arc(x, y, y - 10, beginAngle, endAngle);
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    ctx.fill();
+
+    if (count / total < 0.04) {
+      ctx.font = `6px DejaVu Sans Mono`;
+    } else {
+      ctx.font = `9px DejaVu Sans Mono`;
+    }
+
+    ctx.fillStyle = 'black'
+    ctx.textAlign = 'center'
+    ctx.textBaseline = 'middle'
+    ctx.fillText(`${count} ${name} (${(count / total * 100).toFixed(2)}%)`, x + (Math.cos(beginAngle + angle / 2) * 225), y + (Math.sin(beginAngle + angle / 2) * 225))
+  }
+
+  return canvas.toBuffer();
 }
 
 function getUAInfo() {
@@ -72,7 +142,7 @@ function loadData(uainfo) {
     unknown: 'Unknown',
   };
   var piedata = {};
-  
+
   var i, tot = 0, ua;
   function flag(t, n) {
     piedata[t] += n;
