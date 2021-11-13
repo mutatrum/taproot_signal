1f01d7d8781da1dbe62ef4a7b0a11897590cbf73
mutatrum <mutatrum@gmail.com>
2021-11-13 13:35:00 +0100

    Add activation message

--- taproot_time.js
+++ taproot_time.js
@@ -1,15 +1,15 @@
 "use strict";
 
-const config = require('./config')
-const logger = require('./src/logger')
+const config = require('./config');
+const logger = require('./src/logger');
 
-const BitcoinRpc = require('./src/bitcoin-rpc.js')
-const bitcoin_rpc =  new BitcoinRpc(config.bitcoind)
+const BitcoinRpc = require('./src/bitcoin-rpc.js');
+const bitcoin_rpc =  new BitcoinRpc(config.bitcoind);
 
-const Twitter = require('./src/twitter.js')
-const twitter = new Twitter(config.twitter)
+const Twitter = require('./src/twitter.js');
+const twitter = new Twitter(config.twitter);
 
-const zmq = require('zeromq')
+const zmq = require('zeromq');
 
 const TAPROOT_HEIGHT = 709632;
 const DIFFICULTY_PERIOD = 2016;
@@ -34,6 +34,19 @@ async function onMessage(topic, message) {
 
     var currentHeight = currentBlock.height
     var currentBlockTime = currentBlock.mediantime
+    var currentDifficulty = currentHeight / DIFFICULTY_PERIOD;
+    if (currentHeight == TAPROOT_HEIGHT) {
+      
+      var activationTime = formatDate(new Date((currentBlockTime) * 1000))
+      var text = `🚨 TAPROOT ACTIVATED 🚨\n\nTaproot has been activated in block ${currentHeight}, at difficulty period ${currentDifficulty}, on ${activationTime}.\n\nThis is not a goodbye, my darling, this is a thank you.\n\nSee you all at @anyprevout.`;
+      
+      await twitter.postStatus(text)
+      
+      return
+    }
+    if (currentHeight > TAPROOT_HEIGHT) {
+      return
+    }
     var heightDelta = TAPROOT_HEIGHT - currentHeight
     var previousHeight = currentHeight - heightDelta
     var previousBlockHash = await bitcoin_rpc.getBlockHash(previousHeight)
@@ -44,11 +57,11 @@ async function onMessage(topic, message) {
 
     logger.log(`block ${currentHeight}, taproot activation estimated on ${taprootTime}`)
     
-    var currentDifficulty = currentHeight / DIFFICULTY_PERIOD;
     if (Number.isInteger(currentDifficulty)) {
       var taprootDifficulty = TAPROOT_HEIGHT / DIFFICULTY_PERIOD;
       var difficultyDelta = heightDelta / DIFFICULTY_PERIOD;
       var blocks = '🟧'.repeat(difficultyDelta);
+
       var text = `Taproot activation\n${blocks}\n\nCurrent block: ${currentHeight}, difficulty period: ${currentDifficulty}\nActivation block: ${TAPROOT_HEIGHT}, difficulty period: ${taprootDifficulty}\nBlocks to go: ${heightDelta}, difficulty periods to go: ${difficultyDelta}\nEstimated activation: ${taprootTime}`;
 
       await twitter.postStatus(text)
